e9725d9bdf28a7159ed9b3bbf35cf92fa6bf17d6
parves mosarof <parvesmosarof35@gmail.com>
2026-08-22 21:35:02 +0600

    chore: configure Nixpacks deployment, update environment settings, and cleanup obfuscated code

--- config/envConfig.js
+++ config/envConfig.js
@@ -1,3 +1,9 @@
-export const imgUrl = "https://hostinflu.onrender.com";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  process.env.NEXT_PUBLIC_API_URL ||
+  process.env.NEXT_PUBLIC_IMAGE_URL ||
+  "https://api.hostinflu.com";
+
+export const imgUrl = BASE_URL.replace(/\/+$/, "");
 export const url = `${imgUrl}/api/v1/`;
 export const getBaseUrl = () => url;
